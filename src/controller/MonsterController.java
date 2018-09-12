eddd261039d434bf9f20c2f280fe708efe133826
Bowen <tbow1382@740s-pl-120.local>
2018-09-12 08:07:59 -0600

    added getters and setters

--- src/controller/MonsterController.java
+++ src/controller/MonsterController.java
@@ -1,6 +1,23 @@
 package controller;
 
+import model.MarshmallowMonster;
+
 public class MonsterController
 {
-
+	//Data member section
+	private MarshmallowMonster myMonster;
+	
+	//Constructor section
+	public MonsterController() //this is a default constructor
+	{
+		//Job 1: initialize data members!
+		// put param.s in paren.s
+		myMonster = new MarshmallowMonster("Thelmo", 4, 4, true, 3);
+	}
+	
+	//methods section
+	public void start()
+	{
+		System.out.println(myMonster);
+	}
 }
--- src/model/MarshmallowMonster.java
+++ src/model/MarshmallowMonster.java
@@ -2,5 +2,102 @@ package model;
 
 public class MarshmallowMonster
 {
+	//-----data members section------------
+	//They are ALWAYS private
+	private String name;
+	private double legCount;
+	private int eyeCount;
+	private boolean hasNoses;
+	private int armCount;
+	//------end of data members section------------
+	
 
+	public MarshmallowMonster()
+	{
+		//Default values are 0 or null BORING :(
+	}
+	
+	public MarshmallowMonster(String name)
+	{
+		this.name = name;
+	}
+	
+	public MarshmallowMonster(String name, double legCount, int eyeCount, boolean hasNoses, int armCount)
+	{
+		this.name = name;
+		this.legCount = legCount;
+		this.eyeCount = eyeCount;
+		this.hasNoses = hasNoses;
+		this.armCount = armCount;
+	}
+	
+	
+	//----------Getters----------------
+	public String getName()
+	{
+		return name;
+		// return is an exit method, any code after it is never hit.
+		//put return after all code
+	}
+	
+	public double getLegCount()
+	{
+		return legCount;
+	}
+	
+	public int getEyeCount()
+	{
+		return eyeCount;
+	}
+	
+	public boolean getHasNoses()
+	{
+		return hasNoses;
+	}
+	
+	public int getArmCount()
+	{
+		return armCount;
+	}
+	// -------end of Getters-------------	
+	
+	
+	//----------setters-----------------
+	//setters job is to set/change
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public void setLegCount(double legs)
+	{
+		this.legCount = legs;
+	}
+	
+	public void setEyeCount(int eyes)
+	{
+		this.eyeCount = eyes;
+	}
+	
+	public void setHasNoses(boolean hasNoses)
+	{
+		this.hasNoses = hasNoses;
+	}
+	
+	public void setArmCount(int arms)
+	{
+		this.armCount = arms;
+	}
+	//-----------end of setters--------------
+	
+	public String toString()
+	{
+		String description = "My monsters name is " + name + " he has "
+							+ legCount + " legs"
+							+ " RAWR! XD";
+		
+		
+		return description;
+	}
+	
 }
